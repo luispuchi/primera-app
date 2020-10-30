a8f8ab03b7337be2dac118b6cf4f640f3ad4cac8
luis puchi <puchiluis11@gmail.com>
2020-10-29 22:14:45 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#one'
+  get '/two', to: 'pages#two'
+  get '/three', to: 'pages#three'
+  get '/one', to: 'pages#one'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
